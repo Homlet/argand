7608cec3222c9b9902d71113a8936335f0898b4e
Homletmoo <samlhub@gmail.com>
2014-11-04 11:43:05 +0000

    Add skeleton diagram.py

---
+++ diagram.py
@@ -0,0 +1,38 @@
+"""Argand Diagram Plotter
+
+diagram.py - Stores all information pertaining to
+             a single instance of a diagram.
+
+Written by Sam Hubbard - samlhub@gmail.com
+"""
+
+from geometry import Point
+
+
+class Diagram:
+    def __init__(self):
+        self.plots = []
+        self.zoom = 1.0
+        self.translation = Point(0.0, 0.0)
+        self.preferences = None
+
+    def add_plot(self, plot, index):
+        pass
+    
+    def remove_plot(self, index):
+        pass
+
+    def get_plot(self, index):
+        pass
+
+    def set_zoom(self, zoom):
+        self.zoom = zoom
+
+    def set_translation(self, translation):
+        self.translation = translation
+
+    def translate(self, translation):
+        self.translation += translation
+
+    def set_preferences(self, preferences):
+        self.preferences = preferences
