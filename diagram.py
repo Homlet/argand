e273124f919a4254381275f8e44210e86feb53ea
Homletmoo <samlhub@gmail.com>
2014-12-17 00:53:47 +0000

    Mouse wheel and zoom slider linked.

--- diagram.py
+++ diagram.py
@@ -6,6 +6,7 @@ diagram.py - Stores all information pertaining to
 Written by Sam Hubbard - samlhub@gmail.com
 """
 
+from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 from plot import Plot
@@ -13,8 +14,21 @@ from plot_list import PlotListModel
 from geometry import Point
 
 
-class Diagram:
+class Diagram(QObject):
+    zoom_changed = pyqtSignal(float)
+    translation_changed = pyqtSignal(Point)
+
     def __init__(self):
+        super(Diagram, self).__init__()
+        
         self.plots = PlotListModel()
         self.zoom = 1.0
         self.translation = Point(0.0, 0.0)
+
+    def set_zoom(self, value):
+        self.zoom = value
+        self.zoom_changed.emit(value)
+
+    def set_translation(self, value):
+        self.translation = value
+        self.translation_changed.emit(value)
