fb751d4e02df22ac22ab92e661ad2866e8f62f6a
Homletmoo <samlhub@gmail.com>
2015-02-06 00:41:39 +0000

    Improve documentation for rtf output.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,4 @@
-"""Argand Diagram Plotter
-
-utils.py - Various utility functions.
+"""Various utility functions.
 
 Written by Sam Hubbard - samlhub@gmail.com
 """
@@ -9,12 +7,15 @@ from math import floor
 
 
 def clamp(value, low, high):
+    """Return value clamped to interval [low, high]."""
     return max(low, min(value, high))
 
 
 def floor_to(value, interval):
+    """Return value rounded down to the nearest multiple of interval."""
     return floor(value / interval) * interval
 
 
 def ceil_to(value, interval):
+    """Return value rounded up to the nearest multiple of interval."""
     return floor_to(value + interval, interval)
