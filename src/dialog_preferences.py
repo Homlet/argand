db4ee9d6cecd0dae9b16221791f3174e87d67192
Homletmoo <samlhub@gmail.com>
2015-03-17 13:14:23 +0000

    Remove whitespace in dialog_preferences.py

--- src/dialog_preferences.py
+++ src/dialog_preferences.py
@@ -19,12 +19,12 @@ class DialogPreferences(QDialog):
         self.preferences = preferences
         self.setup_content()
         self.initialize()
-        
+
     def setup_content(self):        
         # Create labels.
         self.stroke_label = QLabel("Stroke width:")
         #self.font_size_label = QLabel("Font size:")
-        
+
         # Create integer inputs.
         self.stroke = QSpinBox()
         self.stroke.setRange(1, 10)
@@ -32,17 +32,17 @@ class DialogPreferences(QDialog):
         #self.font_size = QSpinBox()
         #self.font_size.setRange(12, 24)
         #self.font_size.setValue(self.preferences.font_size)
-        
+
         # Create divider.
         self.divider = QFrame()
         self.divider.setFrameShape(QFrame.VLine)
-        
+
         # Create check-boxes.
         self.label_axes = QCheckBox("Label axes")
         self.label_axes.setChecked(self.preferences.label_axes)
         self.label_points = QCheckBox("Label points")
         self.label_points.setChecked(self.preferences.label_points)
-        
+
         # Create Save and Cancel buttons.
         self.buttons = QDialogButtonBox(
             QDialogButtonBox.Save | QDialogButtonBox.Cancel,
@@ -50,13 +50,13 @@ class DialogPreferences(QDialog):
         )
         self.buttons.accepted.connect(self.accept)
         self.buttons.rejected.connect(self.reject)
-        
+
         # Create a grid layout in the widget.
         grid = QGridLayout()
         grid.setContentsMargins(3, 3, 3, 3)
         grid.setSizeConstraint(QLayout.SetFixedSize)
         self.setLayout(grid)
-        
+
         # Add everything to the grid.
         grid.addWidget(self.stroke_label, 0, 0)
         grid.addWidget(self.stroke, 0, 1)
@@ -66,10 +66,10 @@ class DialogPreferences(QDialog):
         grid.addWidget(self.label_axes, 0, 3)
         grid.addWidget(self.label_points, 1, 3)
         grid.addWidget(self.buttons, 2, 0, 1, 4, Qt.AlignRight)
-    
+
     def initialize(self):
         self.setWindowTitle("Preferences")
-    
+
     def accept(self, *args, **kwargs):
         self.preferences.stroke = self.stroke.value()
         #self.preferences.font_size = self.font_size.value()
